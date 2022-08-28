93ba230553ea46c9bfca48bc6d65355b3264844e
vardhan choudhary <48433744+vardhanwebsite@users.noreply.github.com>
2022-08-28 12:51:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button= document.querySelector('button')
 
-const size=4316534 * 8
+const size=20480  * 8
 const testcase=400;
 const progress= document.querySelector('.progress ')
 const speedtext=document.querySelector('.speed-text')
